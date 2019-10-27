671a7839026b2240bfdd604df80cbb8a123a926f
Nicolás Miranda <48663702+Nimirandad@users.noreply.github.com>
2019-10-27 20:30:01 -0300

    Go

--- lacasonadetoko/home/urls.py
+++ lacasonadetoko/home/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.seccion_nosotros, name='nosotros'),
-    path('', views.seccion_carta, name='carta'),
-    path('', views.seccion_galeria, name='galeria'),
+    path('', views.home, name='home'),
 ]
